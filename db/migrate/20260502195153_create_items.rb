9fb19a85c9c4ddc9f198c35c79bf7d4ff5874931
Rem <r3mnik@gmail.com>
2026-05-02 22:54:36 +0300

    Add storage and items models

---
+++ db/migrate/20260502195153_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[8.1]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.decimal :cost, :decimal, precision: 8, scale: 2
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
