72c072d1ecbb067483bbeecc0246c8aa07ff1b4c
Rem <r3mnik@gmail.com>
2026-05-04 23:46:03 +0300

    Create receiving_items association to receiving model

--- app/models/receiving.rb
+++ app/models/receiving.rb
@@ -1,3 +1,5 @@
 class Receiving < ApplicationRecord
   belongs_to :storage
+  has_many :receiving_items, dependent: :destroy
+  accepts_nested_attributes_for :receiving_items, allow_destroy: true
 end
