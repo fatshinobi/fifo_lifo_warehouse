342d3625a65421a7eae3ac9cb353a43696728008
Rem <r3mnik@gmail.com>
2026-05-05 13:28:27 +0300

    Add validation for received_at and storage_id fields of receivings

--- app/models/receiving.rb
+++ app/models/receiving.rb
@@ -2,4 +2,7 @@ class Receiving < ApplicationRecord
   belongs_to :storage
   has_many :receiving_items, dependent: :destroy
   accepts_nested_attributes_for :receiving_items, allow_destroy: true
+  # Validations to ensure required fields are present
+  validates :received_at, presence: true
+  validates :storage, presence: true
 end
