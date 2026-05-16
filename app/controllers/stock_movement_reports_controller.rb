a48453a4a8a8b506c0fa899213543d12abe6ae4d
Rem <r3mnik@gmail.com>
2026-05-17 00:25:49 +0300

    Add start_time and end_time filters to Stock Movement Report

--- app/controllers/stock_movement_reports_controller.rb
+++ app/controllers/stock_movement_reports_controller.rb
@@ -2,11 +2,15 @@ class StockMovementReportsController < Lintity::EntityReportController
   layout "application"
 
   def index
-    init_fields
+    start_time_value = params[:start_time].presence || 1.month.ago.beginning_of_day
+    end_time_value = params[:end_time].presence || Time.current.end_of_day
+
     @entity_report_header_caption = "Stock Movement Report"
     @records = InventoryTransaction.stock_movement_calculation(
       storage_id: params[:storage_id],
       item_id: params[:item_id],
+      start_time: start_time_value,
+      end_time: end_time_value,
       fields_info: {
         items: { include: :item, field: :name },
         storages: { include: :storage, field: :name }
