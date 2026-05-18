887fa0aa2deca95c97a13b1e93f22ec170a29b2b
Rem <r3mnik@gmail.com>
2026-05-18 14:08:29 +0300

    Show filters desctiption in Stock Balance Report

--- app/controllers/stock_balance_reports_controller.rb
+++ app/controllers/stock_balance_reports_controller.rb
@@ -14,6 +14,25 @@ class StockBalanceReportsController < Lintity::EntityReportController
       }
     )
 
+    # Build a human‑readable description of the applied filters
+    filter_parts = []
+    if storage_id.present?
+      storage_name = Storage.find_by(id: storage_id)&.name
+      filter_parts << "Storage: #{storage_name}" if storage_name.present?
+    end
+    if item_id.present?
+      item_name = Item.find_by(id: item_id)&.name
+      filter_parts << "Item: #{item_name}" if item_name.present?
+    end
+    if params[:balance_time].present?
+      filter_parts << "Balance time: #{balance_time}"
+    end
+    @entity_filter_header_caption = if filter_parts.empty?
+      "Filters: none"
+    else
+      "Filters: #{filter_parts.join(', ')}"
+    end
+
     @entity_report_header_caption = "Stock Balance Report"
   end
 
