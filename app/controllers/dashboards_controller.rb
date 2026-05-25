4e8b7d682b289a08972156abb8d7cc9b694de710
Rem <r3mnik@gmail.com>
2026-05-25 12:03:55 +0300

    Add dashboards boilerplate

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,7 @@
+class DashboardsController < ApplicationController
+  layout "application"
+
+  def index
+    # Add any dashboard data preparation here
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,7 @@ Rails.application.routes.draw do
   resources :events, only: [ :create ]
   # RecalculationsEvents – provides a form (new) and uses the create action from EventsController
   resources :recalculations_events, only: [ :new, :create ]
+
+  # Dashboard route
+  get "dashboard", to: "dashboards#index"
 end
