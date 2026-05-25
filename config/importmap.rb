f5e4e8b38478747c2f80a805a55125693951eefa
Rem <r3mnik@gmail.com>
2026-05-25 22:37:57 +0300

    Add Pie Chart to dashboard

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,7 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 # Correct pin for Popper.js used by Bootstrap dropdowns. The previous alias "@popperjs--core.js" does not export the expected createPopper function.
 pin "@popperjs/core", to: "popper.js", preload: true # @2.11.8
 pin "bootstrap" # @5.3.8
+pin "chartkick" # @5.0.1
+pin "Chart.bundle", to: "Chart.bundle.js"
+pin "chart.js" # @4.5.1
+pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.4
