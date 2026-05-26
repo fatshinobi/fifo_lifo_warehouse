e40143150b87158cc93c3656f642c88c56216d94
Rem <r3mnik@gmail.com>
2026-05-26 12:18:23 +0300

    Sure thet it triggers ReceivingProcess if processed

--- spec/controllers/receivings_controller_spec.rb
+++ spec/controllers/receivings_controller_spec.rb
@@ -19,10 +19,36 @@ RSpec.describe ReceivingsController, type: :controller do
         }
       end
 
+      # New context for processed stock_state
+      let(:processed_params) do
+        {
+          receiving: {
+            storage_id: storage.id,
+            received_at: Time.current,
+            stock_state: "processed",
+            receiving_items_attributes: [
+               { item_id: create(:item).id, qty: 5, cost: 3.0 }
+            ]
+          }
+        }
+      end
+
       it "creates a new Receiving and redirects to index" do
         expect { post :create, params: valid_params }.to change(Receiving, :count).by(1)
         expect(response).to redirect_to(receivings_path)
         expect(flash[:notice]).to eq("Receiving was successfully created.")
+      end
+
+      it "creates a new Receiving with processed state and triggers ReceivingProcess" do
+        # Stub the service to verify it is invoked without executing its internals
+        receiving_instance = instance_double("ReceivingProcess")
+        expect(::ReceivingProcess).to receive(:new).with(an_instance_of(Receiving)).and_return(receiving_instance)
+        expect(receiving_instance).to receive(:call)
+
+        expect { post :create, params: processed_params }.to change(Receiving, :count).by(1)
+        expect(response).to redirect_to(receivings_path)
+        expect(flash[:notice]).to eq("Receiving was successfully created.")
+        expect(Receiving.last.stock_state).to eq("processed")
       end
     end
 
